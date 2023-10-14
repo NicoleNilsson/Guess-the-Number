52c8ca4f2ad2bfd8f9e96d1514f3c21efabccf1a
NicoleNilsson <nicole-nilsson@outlook.com>
2023-10-15 01:40:02 +0200

    -

--- src/GuessingGame.java
+++ src/GuessingGame.java
@@ -25,7 +25,7 @@ public class GuessingGame {
     //metod som initierar och loopar spelet medan playagain är true
     public void startGame(){
         do { 
-            createRandomNumber(100, 100);
+            createRandomNumber(100, 1);
             //för test:
             //System.out.println(randomNumber);            
             System.out.println("Gissa ett tal mellan 1 och 100.");
